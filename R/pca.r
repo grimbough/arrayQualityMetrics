9c074411b7b67fabbb6b4dfdc3db29cf857fc62b
Audrey Kauffmann <audrey@ebi.ac.uk>
2009-02-05 14:39:26 +0000

    added PCA
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/arrayQualityMetrics@37168 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/pca.r
@@ -0,0 +1,23 @@
+aqm.pca = function(expressionset, dataprep, intgroup = "Covariate", ...)
+{
+    covar = pData(expressionset)[colnames(pData(expressionset))==intgroup[1]][,1]
+    colourCovd = brewer.pal(8,rownames(brewer.pal.info[brewer.pal.info$category=="qual",])[6])
+
+    cols = unique(colourCovd[as.factor(covar)])
+
+    pca = prcomp(t(dataprep$dat))
+ 
+    key = list(points = list(pch = 19, col=cols), text = list(levels(as.factor(unlist(covar)))))
+    key$rep = FALSE
+    key$space = "right"
+    pcafig = xyplot(PC2 ~ PC1 , as.data.frame(pca$x), pch=19, col=cols, key = key)
+
+    legend = "Figure <!-- FIG --> represents the Principal Component Analysis of the arrays. The colours correspond to the group of interest given."
+    title = "Principal Component Analysis of the arrays"
+    type = "Between array comparison"
+   
+    out = list("plot" = pcafig, "type" = type,  "title" = title, "legend" = legend, "shape" = "square")
+    class(out) = "aqmobj.pca"
+    return(out) 
+}
+
