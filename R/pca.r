f0c6b169587d522b83f3917119092a50e987c4d3
Audrey Kauffmann <audrey@ebi.ac.uk>
2009-07-16 14:16:13 +0000

    Fix PCA colouring bug found by Matthew Huska.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/arrayQualityMetrics@40650 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pca.r
+++ R/pca.r
@@ -3,11 +3,11 @@ aqm.pca = function(expressionset, dataprep, intgroup = "Covariate", ...)
     covar = pData(expressionset)[colnames(pData(expressionset))==intgroup[1]][,1]
     colourCovd = brewer.pal(8,rownames(brewer.pal.info[brewer.pal.info$category=="qual",])[6])
 
-    cols = unique(colourCovd[as.factor(covar)])
+    cols = colourCovd[as.factor(covar)]
 
     pca = prcomp(t(dataprep$dat))
  
-    key = list(points = list(pch = 19, col=cols), text = list(levels(as.factor(unlist(covar)))))
+    key = list(points = list(pch = 19, col=unique(cols)), text = list(levels(as.factor(unlist(covar)))))
     key$rep = FALSE
     key$space = "top"
     pcafig = xyplot(PC2 ~ PC1 , as.data.frame(pca$x), pch=19, col=cols, key = key)
