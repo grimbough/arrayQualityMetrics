ba466b9666e97fcc94af17f6e815d63dae0dcecc
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2010-08-13 15:39:17 +0000

    CLeaning up some of the spaghetti code.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/arrayQualityMetrics@48797 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/boxplot.r
+++ R/boxplot.r
@@ -1,72 +1,95 @@
+
+within = function(x, range) {
+  stopifnot(length(range)==2)
+  return((x >= range[1]) & (x <= range[2]))
+}
+
 aqm.boxplot = function(expressionset, dataprep, intgroup = "Covariate", grouprep = FALSE, ...)
 {
 
-  b = boxplot(dataprep$dat, plot=FALSE, range=0)  
-  bmeanstat = boxplot.stats(b$stat[3,])
-  bmeanout = sapply(seq_len(length(bmeanstat$out)), function(x) which(b$stat[3,] == bmeanstat$out[x]))
-  
+  b = boxplot(dataprep$dat, plot=FALSE, range=0)
+
+  ## Outlier detection, applied to the boxplot statistics: to the medians (i.e. box positions),
+  ## and differences between upper hinge and lower hinge (i.e. box sizes)
+  bmedian = b$stat[3,]
+  bmedianstat = boxplot.stats(bmedian)
   biqr = b$stat[4,] -  b$stat[2,] 
   biqrstat = boxplot.stats(biqr)
-  biqrout = sapply(seq_len(length(biqrstat$out)), function(x) which(biqr == biqrstat$out[x]))
 
-  outliers = unlist(c(bmeanout, biqrout))
+  bmedianout = !within(bmedian, bmedianstat$stats[c(1,5)])
+  biqrout    = !within(biqr,    biqrstat$stats[c(1,5)])
+  outliers   = bmedianout | biqrout
+
   
-  if(dataprep$nchannels == 2)
-    {
-      bsrc = boxplot(dataprep$rc, plot=FALSE)
-      bsgc = boxplot(dataprep$gc, plot=FALSE)
-      bsdat = boxplot(dataprep$dat, plot=FALSE)
-          
-      bigdat = c(as.numeric(bsrc$stats), as.numeric(bsgc$stats), as.numeric(bsdat$stats))
-      colbigd = c(as.vector(col(bsrc$stat)), as.vector(col(bsgc$stats)), as.vector(col(bsdat$stats)))
-      fac = c(rep("a. Red Channel",dim(bsrc$stats)[1]*dim(bsrc$stats)[2]), rep("b. Green Channel",dim(bsgc$stats)[1]*dim(bsgc$stats)[2]), rep("c. Log(Ratio)",dim(bsdat$stats)[1]*dim(bsdat$stats)[2]))
+  bsdat   = boxplot(dataprep$dat, plot=FALSE)
 
-    }
-  else {
-    bsdat = boxplot(dataprep$dat, plot=FALSE)
-    bigdat = as.numeric(bsdat$stats)
+  stopifnot( dataprep$nchannels %in% c(1,2) )
+  if(dataprep$nchannels == 2)  {
+    bsrc = boxplot(dataprep$rc, plot=FALSE)
+    bsgc = boxplot(dataprep$gc, plot=FALSE)
+    
+    bigdat  = c(as.numeric(bsrc$stats), as.numeric(bsgc$stats), as.numeric(bsdat$stats))
+    colbigd = c(as.vector(col(bsrc$stat)), as.vector(col(bsgc$stats)), as.vector(col(bsdat$stats)))
+    fac = c(rep("a. Red Channel",   prod(dim(bsrc$stats ))),
+            rep("b. Green Channel", prod(dim(bsgc$stats ))),
+            rep("c. Log(Ratio)",    prod(dim(bsdat$stats))))
+  } else {
+    bigdat  = as.numeric(bsdat$stats)
     colbigd = as.vector(col(bsdat$stats))
   }
   
-  if(all(intgroup %in% names(phenoData(expressionset)@data)) && grouprep == TRUE)
-    {
-      gp = intgroup[1]
-      gpcont = pData(expressionset)[colnames(pData(expressionset))==gp]
-      coloursb = brewer.pal(8,rownames(brewer.pal.info[brewer.pal.info$category=="qual",])[6])
-      intgroupcont = gpcont[,gp]
-      colsb = coloursb[as.factor(intgroupcont)]
-      if(length(outliers) != 0)
-        colsb[outliers] = "grey"
+  if(all(intgroup %in% names(phenoData(expressionset)@data)) && grouprep) {
+    gp = intgroup[1]
+    gpcont = pData(expressionset)[colnames(pData(expressionset))==gp,]
+    coloursb = brewer.pal(8, rownames(brewer.pal.info[brewer.pal.info$category=="qual",])[6])
+    intgroupcont = gpcont[,gp]
+    colsb = coloursb[as.factor(intgroupcont)]
+    colsb[outliers] = "grey"
      
-      key = list(rect = list(col=unlist(coloursb)[as.factor(levels(as.factor(unlist(gpcont[,1]))))]), text = list(levels(as.factor(unlist(gpcont[,1])))))
-      key$rep = FALSE
-  
-      foo = draw.key(key = key)
+    key = list(rect = list(col=unlist(coloursb)[as.factor(levels(as.factor(unlist(gpcont[,1]))))]),
+               text = list(levels(as.factor(unlist(gpcont[,1])))))
+    key$rep = FALSE
+    foo = draw.key(key = key)
+    
+  } else {
+    colsb = rep("#1F78B4", dataprep$numArrays) 
+    colsb[outliers] = "grey"
+    
+    foo = NULL
+  }
+    
+  if(dataprep$nchannels == 2) {
+    formula = colbigd ~ bigdat | factor(fac)
+    lay = c(3,1)
+  } else {
+    formula = colbigd ~ bigdat
+    lay = c(1,1)
+  }  
 
-      if(dataprep$nchannels == 2)
-        {
-          box = bwplot(colbigd ~ bigdat | factor(fac),  horizontal=TRUE, groups = colbigd, layout=c(3,1), as.table=TRUE, strip = function(..., bg) strip.default(..., bg ="#cce6ff"), pch = "|",  col = "black", do.out = FALSE, box.ratio = 2, xlab = "", fill = colsb, panel = panel.superpose, panel.groups = panel.bwplot, main=foo) #
-        }
-      else {
-        box = bwplot(colbigd ~ bigdat, horizontal=TRUE,  groups = colbigd, pch = "|",  col = "black", do.out = FALSE, box.ratio = 2, xlab = "", fill = colsb, panel = panel.superpose, panel.groups = panel.bwplot, main=foo, ylab = "")
-      }
-    } else {
-      colsb = rep("#1F78B4",dataprep$numArrays) #
-      if(length(outliers) != 0)
-        colsb[outliers] = "grey"
-      box = if(dataprep$nchannels == 2) bwplot(colbigd ~ bigdat | factor(fac),  horizontal=TRUE, groups = colbigd, layout=c(3,1), as.table=TRUE, strip = function(..., bg) strip.default(..., bg ="#cce6ff"), pch = "|",  col = "black", do.out = FALSE, box.ratio = 2, xlab = "", fill = colsb, panel = panel.superpose, panel.groups = panel.bwplot) else bwplot(colbigd ~ bigdat,  horizontal=TRUE, groups = colbigd, pch = "|",  col = "black", do.out = FALSE, box.ratio = 2, xlab = "", fill = colsb, panel = panel.superpose, panel.groups = panel.bwplot, ylab = "") #
-    }
+  box = bwplot(formula, layout=lay, as.table=TRUE,
+        strip = function(..., bg) strip.default(..., bg ="#cce6ff"),
+        horizontal = TRUE, groups = colbigd, 
+        pch = "|",  col = "black", do.out = FALSE, box.ratio = 2,
+        xlab = "", fill = colsb, panel = panel.superpose, panel.groups = panel.bwplot, main=foo)
+
 
   shape = list("h" = if(dataprep$numArrays > 50) (dataprep$numArrays/8) else 10, "w" = 10) 
   
-  legspe = if(dataprep$nchannels == 2) "The left panel corresponds to the red channel. The middle to the green channel. The right panel shows the boxplots of log<sub>2</sub>(ratio)." else ""
+  legspe = if(dataprep$nchannels == 2) "Left panel: red channel, middle panel: green channel, right panel: log<sub>2</sub>(ratio)." else ""
   
   legend = sprintf("The figure <!-- FIG --> presents boxplots of the log<sub>2</sub>(Intensities). Each box corresponds to one array. %s It gives a simple summary of the distribution of probe intensities across all arrays. Typically, one expects the boxes to have similar size (IQR) and y position (median). If the distribution of an individual array is very different from the others, this may indicate an experimental problem. After normalisation, the distributions should be similar.", legspe)
 
   title = "Boxplots"
   section = "Array intensity distributions"
 
-  out = list("plot" = box, "section" = section, "title" = title, "legend" = legend, "scores" = list("mean" = b$stat[3,], "IQR" = biqr), "outliers" = list("mean" = bmeanout, "IQR" = biqrout), "shape" = shape)
+  out = list("plot" = box,
+             "section" = section,
+             "title" = title,
+             "legend" = legend,
+             "scores"   = list("median" = bmedian, "IQR" = biqr),
+             "outliers" = list("median" = bmedianout, "IQR" = biqrout),
+             "shape" = shape)
   class(out) = "aqmobj.box"
+  
   return(out)   
 }
