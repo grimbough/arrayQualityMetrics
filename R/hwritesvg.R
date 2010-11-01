e36f0532262faa81e48a9e2fa17346581ffcdb86
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2010-11-01 12:08:19 +0000

    Fixes to the CSS file.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/arrayQualityMetrics@50593 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/hwritesvg.R
+++ R/hwritesvg.R
@@ -22,19 +22,19 @@ aqm.hwriteImage = function (image.url, page = NULL, ..., image.border = 0, width
     }
 
     getExtension = function(x) { s = strsplit(x, split=".", fixed=TRUE)[[1]] ; s[length(s)] }
-      
+    alt = paste(image.url, "appears to be missing or not renderable by this browser.")
+    
     str = switch(getExtension(image.url),
       "svg" = {
-        alt = "Your browser does not seem to support embedding of SVG objects; consider using Firefox, Safari, Opera, IE >=9."
+        alt = paste(alt, "To render SVG, consider using Firefox, Safari, Opera or IE >=9."
         ## This is ugly: in order to avoid confusion with hmakeTag's own 'data' argument
         ##   I use DATA, using the fact that R is case-sensitive while HTML is not.
         ## I also put the 'alt' text both in the alt attribute and as text between <object..> and </object>,
-        ##   IE does not seem to honour the alt attribute.
+        ##   as IE does not seem to honour the alt attribute.
         hwriter::hmakeTag("object", type="image/svg+xml", DATA = image.url, border = image.border, 
                           alt = alt, data = alt, width = width, height = height)
       }, {
       ## default:
-        alt = paste(image.url, "appears to be missing or not renderable by this browser.")
         hwriter::hmakeTag("img", src = image.url, border = image.border, 
                           alt = alt, width = width, height = height)
       })
